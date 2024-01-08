15afed0786c9ddf29dd27b149d91efcba4c54b42
VitalyNes <v.nesmely@gmail.com>
2024-01-08 13:08:49 +0200

    Видалення зайвих коментарів

--- scripts/mint.js
+++ scripts/mint.js
@@ -4,10 +4,10 @@ async function main() {
   // Вибір контракту та рахунку для мінтингу
   const [deployer] = await hre.ethers.getSigners();
   const Token = await hre.ethers.getContractFactory("VINCoin");
-  const token = await Token.attach("0x62f1EFD105aB29674ADd9BcC3Ff17305040d685e"); // Адреса вашого контракту
+  const token = await Token.attach("0x62f1EFD105aB29674ADd9BcC3Ff17305040d685e"); 
 
   // Мінтинг токенів
-  const mintTx = await token.mint(deployer.address, hre.ethers.parseUnits("1000", 14)); // Мінтинг 1000 токенів
+  const mintTx = await token.mint(deployer.address, hre.ethers.parseUnits("1000", 14)); 
   await mintTx.wait();
   
   console.log(`Мінтинг 1000 токенів на адресу ${deployer.address}`);
